e8cdcf722f8ee32706d186507e212fc0ce889d2c
Cynthia <cynthia.tedesco@gmail.com>
2017-05-26 09:13:53 -0300

    firsts ejs

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app      = express();                               // create our app w/ exp
 // var mongoose = require('mongoose');                     // mongoose for mongodb
 var morgan = require('morgan');             // log requests to the console (express4)
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+// var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var combinatorial = require('./combinatorial.js');
 
 // configuration =================
@@ -12,24 +12,28 @@ var combinatorial = require('./combinatorial.js');
 // mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
 
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+// views is directory for all template files
+app.set('views', __dirname + '/views');
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride());
+// app.use(methodOverride());
+app.set('view engine', 'ejs');
 
 // listen (start app with node server.js) ======================================
-app.listen(2000);
-console.log("App listening on port 2000");
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
 
 // routes ======================================================================
-
 app.get('/', function(req, res) {
     res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 app.get('/paths', function(req, res) {
     console.log('inside serveeeer');
-    res.send(combinatorial.getFirstMinPaths());
-    // console.log();
+    res.render('./paths.ejs');
+    // res.send(combinatorial.getFirstMinPaths());
 });
